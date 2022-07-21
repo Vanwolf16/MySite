863b8c019cbe4b3808219b6cc4c1b446d06fede2
vanwolf16 <d.murillo28@yahoo.com>
2022-07-21 13:38:56 -0700

    Register Done

--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import NewUserForm
 # Create your views here.
 
 def register(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('myapp/products')
     form = NewUserForm()
     context = {
         'form':form,
