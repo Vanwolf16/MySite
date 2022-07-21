3c656cdbc1091e0d91f7ff4b00e78518f9dc666c
vanwolf16 <d.murillo28@yahoo.com>
2022-07-21 11:47:43 -0700

    Created a Register Page

---
+++ users/forms.py
@@ -0,0 +1,14 @@
+from dataclasses import fields
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True,widget=forms.EmailInput({'class':'focus:outline-none','placeholder':'demo@gmail.com'}))
+    username = forms.CharField(required=True,widget=forms.TextInput({'class':'focus:outline-none','placeholder':'wolfie'}))
+    password1 = forms.CharField(required=True,widget=forms.PasswordInput({'class':'focus:outline-none','placeholder':'password'}))
+    password2 = forms.CharField(required=True,widget=forms.PasswordInput({'class':'focus:outline-none','placeholder':'confirm password'}))
+
+    class Meta:
+        model = User
+        fields = ("username","email","password1","password2")
